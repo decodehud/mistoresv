0729ce1ff564c22300be291ea4a38fc235790be6
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-06-04 23:57:44 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Layout from '@/components/layout'
 
 export default function Home() {
   return (
-    <div className='font-bold underline'>
-      hola 
-    </div>
+   
+   <Layout title="home">
+    home page
+   </Layout>
+    
   )
 }
